a32e49b7480933bcc3d67d8d862da985330e3741
Darrin Barker <darrin.barker@flatironschool.com>
2023-02-17 20:16:21 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+ return  drivers.filter(driver => name.toLowerCase() === driver.toLowerCase() )
+  
+}
+
+const fuzzyMatch = (drivers, names) => {
+    const namesLength = names.length;
+    return drivers.filter(a => {
+        return a.slice(0,namesLength) === names;
+    })
+} 
+
+const matchName = (drivers, names) => {
+    return drivers.filter(q => {
+        return q.name === names;
+    })
+}
